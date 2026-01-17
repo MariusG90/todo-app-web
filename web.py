6cda2846e71884d537b03f6683976398f3189ab2
Marius Grosariu <Marius.Grosariu@partner.bmw.de>
2026-01-17 14:51:08 +0200

    Fixing a bug

--- web.py
+++ web.py
@@ -4,19 +4,25 @@ import functions
 todos = functions.get_todos()
 
 def add_todo():
-    todo = st.session_state["new_todo"] + "\n"
-    todos.append(todo)
-    functions.set_todos(todos)
+    todo_text = st.session_state["new_todo"].strip()
+    if todo_text:
+        todos.append(todo_text + "\n")  # add newline for the file
+        functions.set_todos(todos)
+        st.session_state["new_todo"] = ""
 
 st.title("My Todo App")
 
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
+    todo_clean = todo.rstrip("\n")
+    checkbox = st.checkbox(todo_clean, key=f"{todo_clean}_{index}")
     if checkbox:
         todos.pop(index)
         functions.set_todos(todos)
-        del st.session_state[todo]
         st.rerun()
 
-st.text_input(label=" ", placeholder="Add new todo...",
-              on_change=add_todo, key="new_todo")
+st.text_input(
+    label=" ",
+    placeholder="Add new todo...",
+    on_change=add_todo,
+    key="new_todo"
+)
